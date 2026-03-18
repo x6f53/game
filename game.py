ae672cc5d1e323f919600b3f45dd205320151254
Drahomir Sabanos <x6f53@students.keele.ac.uk>
2026-03-18 21:31:24 +0000

    Add loop to allow multiple guesses

--- game.py
+++ game.py
@@ -2,9 +2,11 @@ import random
 
 number = random.randint(1, 5)
 
-guess = int(input("Guess a number between 1 and 5: "))
-
-if guess == number:
-    print("Correct!")
-else:
-    print("Wrong! The number was", number)
+while True:
+    guess = int(input("Guess a number between 1 and 5: "))
+    
+    if guess == number:
+        print("Correct!")
+        break
+    else:
+        print("Try again")

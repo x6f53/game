cf7bfa036e0cb12e8662bdf1bb7820928c3d2f71
x6f53 <x6f53@students.keele.ac.uk>
2026-03-22 15:45:49 +0000

    Update game.py
    added acknowledgement

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+#I acknowledge the use of
+#Microsoft Copilot (version GPT-4, Microsoft, https://copilot.microsoft.com/)
+#to create the code in this file
 import random
 
 number = random.randint(1, 5)
